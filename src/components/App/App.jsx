600c9f316a0cb10fcbd51134e7234afef351643d
Julia-Gavrashenko <110378938+Julia-Gavrashenko@users.noreply.github.com>
2023-03-08 17:49:42 +0200

    Initial commit

---
+++ src/components/App/App.jsx
@@ -0,0 +1,77 @@
+import { Component } from 'react';
+
+import { ContactFilter } from 'components/ContactFilter/ContactFilter';
+import { ContactForm } from '../ContactForm/ContactForm';
+import { ContactList } from '../ContactList/ContactList';
+import { GlobalStyle } from '../GlobalStyle';
+import { ContactListTitle, FormTitle, ContactsLayout } from './App.styled';
+
+export class App extends Component {
+  state = {
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
+  };
+
+  addContact = newContact => {
+    const existedContact = this.state.contacts.find(
+      contact => contact.name.toLowerCase() === newContact.name.toLowerCase()
+    );
+
+    existedContact
+      ? alert('This contact is already in contacts.')
+      : this.setState(prevState => {
+          return {
+            contacts: [...prevState.contacts, newContact],
+          };
+        });
+  };
+
+  deleteContact = contactId => {
+    this.setState(prevState => {
+      return {
+        contacts: prevState.contacts.filter(
+          contact => contact.id !== contactId
+        ),
+      };
+    });
+  };
+
+  changeFilter = event => {
+    this.setState({ filter: event.currentTarget.value });
+  };
+
+  getFilteredContacts = () => {
+    const { filter, contacts } = this.state;
+    const normalizedFilter = filter.toLowerCase();
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(normalizedFilter)
+    );
+  };
+
+  render() {
+    const filteredContacts = this.getFilteredContacts();
+
+    return (
+      <ContactsLayout>
+        <FormTitle>Phonebook</FormTitle>
+        <ContactForm onAddContact={this.addContact} />
+        <ContactListTitle>Contacts</ContactListTitle>
+        <ContactFilter
+          filter={this.state.filter}
+          onChange={this.changeFilter}
+        />
+        <ContactList
+          contacts={filteredContacts}
+          onDeleteContact={this.deleteContact}
+        />
+        <GlobalStyle />
+      </ContactsLayout>
+    );
+  }
+}
+
