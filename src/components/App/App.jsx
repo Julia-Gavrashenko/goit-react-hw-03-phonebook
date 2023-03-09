bcbf9a180ff501124e0f31c69087ac3225a25a15
Julia-Gavrashenko <julia.gavrashenko@gmail.com>
2023-03-09 17:27:43 +0200

    second-commit

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -5,14 +5,15 @@ import { ContactForm } from '../ContactForm/ContactForm';
 import { ContactList } from '../ContactList/ContactList';
 import { GlobalStyle } from '../GlobalStyle';
 import { ContactListTitle, FormTitle, ContactsLayout } from './App.styled';
+import initialContacts from  'data/contacts.json';
 
 export class App extends Component {
   state = {
     contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+      // { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      // { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      // { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      // { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
     ],
     filter: '',
   };
@@ -21,8 +22,10 @@ export class App extends Component {
     const savedContacts = localStorage.getItem('contacts')
     if (savedContacts !== null) {
       const parsedContacts = JSON.parse(savedContacts)
-      this.setState({contacts: parsedContacts })
+      this.setState({ contacts: parsedContacts })
+      return
     }
+    this.setState({ contacts: initialContacts })
   }
 
 
